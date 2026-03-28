33044a3a05273e2c43e8d361d160319465f51419
Neotisk <admin@neotisk.com>
2026-03-27 22:03:47 -0400

    Add merchants module with controller and service for merchant management

--- payment-service/src/app.module.ts
+++ payment-service/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransactionsModule } from './modules/transactions/transactions.module';
 import { SettlementsModule } from './modules/settlements/settlements.module';
 import { PrismaModule } from './prisma.module';
+import { MerchantsModule } from './modules/merchants/merchants.module';
 
 @Module({
-	imports: [PrismaModule, TransactionsModule, SettlementsModule],
+	imports: [PrismaModule, TransactionsModule, SettlementsModule, MerchantsModule],
 	controllers: [],
 	providers: [],
 })
